8c2d341b94e668ac502c611bfed94b4d741666c0
don fong <djfong@google.com>
2017-04-28 15:55:46 -0700

    simplify initDB().

--- db.go
+++ db.go
@@ -7,10 +7,5 @@ import (
 // initDB opens the named database and returns a handle wrapper.
 func initDB(dbName string) (dbType, error) {
 	h, err := sql.Open("sqlite3", dbName)
-	if err != nil {
-		return dbType{}, err
-	}
-
-	// assign the global db variable
-	return dbType{handle: h}, nil
+	return dbType{handle: h}, err
 }
