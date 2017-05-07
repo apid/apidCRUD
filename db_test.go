1476cd7b0c0f5b2f91caa634ffa0816b7f5f09d3
don fong <djfong@google.com>
2017-05-07 12:31:22 -0700

    simplify db setup for testing.

--- db_test.go
+++ db_test.go
@@ -2,9 +2,8 @@ package apidCRUD
 
 import (
 	"fmt"
+	"os"
 	"database/sql"
-	// _ "github.com/proullon/ramsql/driver"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 const ut_DBTYPE = "sqlite3"
@@ -14,7 +13,7 @@ const ut_DBNAME = "unit-test.db"
 // note that tests using this DB are not true unit tests,
 // since they are implicitly filesystem dependent.
 func fakeInitDB() (dbType, error) {
-	
+	os.Remove(ut_DBNAME)
 	dbHandle, err := sql.Open(ut_DBTYPE, ut_DBNAME)
 	db = dbType{dbHandle}	// assigns to global
 	if err == nil {
@@ -25,9 +24,6 @@ func fakeInitDB() (dbType, error) {
 
 var cmds = []string {
 	// ordinary tables
-	`drop table if exists tables`,
-	`drop table if exists bundles`,
-	`drop table if exists users`,
 	`create table tables(name text unique not null)`,
 	`create table bundles(id integer not null primary key autoincrement, name text not null, uri text not null)`,
 	`insert into bundles (name, uri) values ("b1", "http://localhost/~dfong/bundles/b1.zip")`,
@@ -38,7 +34,6 @@ var cmds = []string {
 	`insert into tables (name) values ("nothing")`,
 
 	// xxx is an extra scratch table
-	`drop table if exists xxx`,
 	`create table xxx(id integer not null primary key autoincrement, name text not null, uri text not null)`,
 	`insert into xxx (name, uri) values ("x1", "url1")`,
 	`insert into xxx (name, uri) values ("x2", "url2")`,
@@ -48,7 +43,6 @@ var cmds = []string {
 	`insert into xxx (name, uri) values ("x6", "url6")`,
 
 	// create table for testing behavior around maxRecs
-	`drop table if exists toomany`,
 	`create table toomany(id integer not null primary key autoincrement, name text not null, uri text not null)`,
 	`insert into toomany (name, uri) values ("x1", "url1")`,
 	`insert into toomany (name, uri) values ("x2", "url2")`,
